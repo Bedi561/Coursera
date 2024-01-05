52c2990254d6efc200c1119f1827c5ed5a5ec035
Bedi561 <110516777+Bedi561@users.noreply.github.com>
2024-01-05 17:51:02 +0530

    hello

--- server/index.js
+++ server/index.js
@@ -8,6 +8,22 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+const express = require('express');
+
+// Enable CORS for all routes
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://starlit-cuchufli-7fe773.netlify.app');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
+// Your routes and middleware go here...
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
+
 
 app.use("/admin", adminRouter)
 app.use("/user", userRouter)
