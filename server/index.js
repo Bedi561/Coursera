223c7493c3ff2466d413b319a1252b749ab6b9d8
Bedi561 <110516777+Bedi561@users.noreply.github.com>
2024-01-05 17:55:14 +0530

    hello1

--- server/index.js
+++ server/index.js
@@ -6,40 +6,24 @@ const userRouter = require("./routes/user");
 
 const app = express();
 
+// Enable CORS for all routes
 app.use(cors());
 app.use(express.json());
-const express = require('express');
 
-// Enable CORS for all routes
-app.use((req, res, next) => {
+// Your routes and middleware go here...
+app.use("/admin", adminRouter);
+app.use("/user", userRouter);
+
+// Handle preflight requests for '/admin/me'
+app.options('/admin/me', (req, res) => {
   res.header('Access-Control-Allow-Origin', 'https://starlit-cuchufli-7fe773.netlify.app');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
+  res.send();
 });
-
-// Handle preflight requests
-app.options('/admin/me', (req, res) => {
-    res.header('Access-Control-Allow-Origin', 'https://starlit-cuchufli-7fe773.netlify.app');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    res.send();
-  });
-  
-
-// Your routes and middleware go here...
-
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
-});
-
-
-app.use("/admin", adminRouter)
-app.use("/user", userRouter)
-
 
 // Connect to MongoDB
-// DONT MISUSE THIS THANKYOU!!
 mongoose.connect('mongodb+srv://pranavbedi6:HePaj9j1QpC0OTcw@cluster0.k0ungut.mongodb.net/', { useNewUrlParser: true, useUnifiedTopology: true, dbName: "courses" });
 
+// Start the server on port 3001
 app.listen(3001, () => console.log('Server running on port 3001'));
