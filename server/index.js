ac8fb2f0c65a8a1ed9ebc663b77f0dfa0bc2a1ad
Bedi561 <110516777+Bedi561@users.noreply.github.com>
2024-01-05 20:34:34 +0530

    every14322

--- server/index.js
+++ server/index.js
@@ -3,37 +3,28 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const adminRouter = require("./routes/admin");
 const userRouter = require("./routes/user");
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
 // Enable CORS for all routes
 app.use(cors());
 
-const prodOrigins = [process.env.ORIGIN_1, process.env.ORIGIN_2]
-const devOrigin = ['http://localhost:5173', ]
-const allowedOrigins = process.env.NODE_ENV === 'production' ? prodOrigins: devOrigin
-app.use( cors({
-    origin: (origin, callback) => {
-        if(allowedOrigins.includes(origin)){
-            console.log(origin, allowedOrigins)
-            callback(null, true);
-        } else{
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-    credentials: true,
-    methods: ['GET','POST','PUT','DELETE']
+const devOrigin = ['http://localhost:5173'];
+const allowedOrigins = process.env.NODE_ENV === 'production' ? devOrigin : devOrigin;
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if (allowedOrigins.includes(origin)) {
+      console.log(origin, allowedOrigins);
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE']
 }));
-
-
-
-
-
-
-
-
-
 
 app.use(express.json());
 
@@ -41,15 +32,8 @@ app.use(express.json());
 app.use("/admin", adminRouter);
 app.use("/user", userRouter);
 
-
-
-
 // Connect to MongoDB
 mongoose.connect('mongodb+srv://pranavbedi6:HePaj9j1QpC0OTcw@cluster0.k0ungut.mongodb.net/', { useNewUrlParser: true, useUnifiedTopology: true, dbName: "courses" });
 
 // Start the server on port 3001
 app.listen(3001, () => console.log('Server running on port 3001'));
-
-
-
-
