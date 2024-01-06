bb77f7d80897ab991ac74e31cc3d81f181381117
Bedi561 <110516777+Bedi561@users.noreply.github.com>
2024-01-06 15:01:16 +0530

    changes1122

--- server/index.js
+++ server/index.js
@@ -9,16 +9,16 @@ const app = express();
 
 app.use(function(req, res, next) {
     // res.header("Access-Control-Allow-Origin", "*");
-    const allowedOrigins = [ 'https://coursera-ebon.vercel.app/', 'http://localhost:5173'];
+    const allowedOrigins = ['https://coursera-ebon.vercel.app', 'http://localhost:5173'];
     const origin = req.headers.origin;
     if (allowedOrigins.includes(origin)) {
-         res.setHeader('Access-Control-Allow-Origin', origin);
+        res.setHeader('Access-Control-Allow-Origin', origin);
     }
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    res.header("Access-Control-Allow-credentials", true);
+    res.header("Access-Control-Allow-Credentials", true);
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
     next();
-  });
+});
 
 
 
