bff59520605969fe7cddbaf40e6e4e03b165a229
Bedi561 <110516777+Bedi561@users.noreply.github.com>
2024-02-21 17:06:00 +0530

    last1

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ const allowedOrigins = ['https://coursera-ebon.vercel.app', 'http://localhost:51
 
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://coursera-ebon.vercel.app', 'https://coursera-orcin.vercel.app/');
+    res.header('Access-Control-Allow-Origin', 'https://coursera-last.vercel.app/', 'https://coursera-orcin.vercel.app/');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
