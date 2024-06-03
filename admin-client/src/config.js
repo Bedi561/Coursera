a603f5d720fe0ae15096654267df40db4ea131f8
Bedi561 <110516777+Bedi561@users.noreply.github.com>
2024-06-03 13:16:53 +0530

    hii

--- admin-client/src/config.js
+++ admin-client/src/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://localhost:3001';
-// export const BASE_URL = 'https://coursera-v1jf.onrender.com';
+// export const BASE_URL = 'http://localhost:3001';
+export const BASE_URL = 'https://coursera-clone-nm8f.onrender.com';
 
 // export const BASE_URL = 'https://coursera-v1jf.onrender.com';
